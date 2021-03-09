02a2d4fc2e1316bb567dd67ce924d453fa71c637
Branton Hung <BHung@lcs.on.ca>
2021-03-09 11:37:36 -0500

    Added location type

---
+++ Shared/Model/Location.swift
@@ -0,0 +1,27 @@
+//
+//  Location.swift
+//  LakefieldMap1
+//
+//  Created by Branton Hung on 2021-03-09.
+//
+
+import Foundation
+
+struct Location: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+
+
+static let example = Location(id: 1,
+                              name: "Lakefield College School",
+                              description: "Lakefield College School is a private day and boarding school located north of the village of Lakefield, Ontario.",
+                              latitude: 44.43948652171136,
+                              longitude: -78.26544785999143,
+                              heroPicture: "highlands"
+                             )
+}
+                              

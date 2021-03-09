6f2e4a9bef6fec821d35cad63e7e80bf9dcd882d
Branton Hung <BHung@lcs.on.ca>
2021-03-09 11:28:58 -0500

    Initial Commit

---
+++ Shared/LakefieldMap1App.swift
@@ -0,0 +1,17 @@
+//
+//  LakefieldMap1App.swift
+//  Shared
+//
+//  Created by Branton Hung on 2021-03-09.
+//
+
+import SwiftUI
+
+@main
+struct LakefieldMap1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

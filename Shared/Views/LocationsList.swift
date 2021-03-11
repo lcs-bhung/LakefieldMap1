a67edd7a68d95602d5d0b9db8cd500f412a4f880
Branton Hung <BHung@lcs.on.ca>
2021-03-11 11:18:57 -0500

    Navigation Links now work

--- Shared/Views/LocationsList.swift
+++ Shared/Views/LocationsList.swift
@@ -31,7 +31,7 @@ struct LocationsList: View {
                         }
                     }
                 }
-            navigationTitle("Locations")
+            .navigationTitle("Locations")
         
         }
     }

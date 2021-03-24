31d179f0238b90c8ecca649de522c428f10c8bf3
Branton Hung <BHung@lcs.on.ca>
2021-03-24 10:50:15 -0400

    Finished app

--- Shared/Views/LocationsList.swift
+++ Shared/Views/LocationsList.swift
@@ -18,7 +18,6 @@ struct LocationsList: View {
     var body: some View {
         
  
-        
         VStack{
         
             SearchBarView(text: $searchText)
@@ -41,51 +40,10 @@ struct LocationsList: View {
                              }
                          }
                      }
-                 navigationTitle("Locations")
              
              }
         }
-//        VStack{
-//
-//            //Include the search bar view
-//
-//            SearchBarView(text: $searchText)
-//
-//            List {
-//                ForEach(store.places.filter({ filter in
-//                    if !searchText.isEmpty {
-//                        return
-//                            filter.name.lowercased().contains(searchText.lowercased())
-//                    } else {
-//                        return true
-//                    }
-//                }
-//                )) {
-//                    location in
-//                    NavigationLink(destination: LocationDetail(location: location)){
-//                        HStack{
-//                            Image(location.logo)
-//                                .resizable()
-//                                .scaledToFit()
-//                                .frame(width: 44, height: 44)
-//
-//                            HStack {
-//                                Text(location.name)
-//                                    .font(.subheadline)
-//
-//                            }
-//                        }
-//
-//
-//
-//                    }
-//
-//
-//                    .navigationTitle("Locations")
-//                }
-//            }
-//
-//        }
+        .navigationTitle("Locations")
         
     }
     
